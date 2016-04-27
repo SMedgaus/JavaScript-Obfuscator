b85bf60897b9a48cf08254a84cf9b2f6945dbcdc
Sergey Medgaus <medgaus-sergey@yandex.ru>
2016-04-27 20:46:17 +0300

    Simple interface is added, providing simple minification with the help of Esprima.

--- src/obfuscating/Obfuscator.java
+++ src/obfuscating/Obfuscator.java
@@ -8,6 +8,7 @@ package obfuscating;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Properties;
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -26,12 +27,20 @@ final public class Obfuscator {
     private final JSObject escodegen;
 
     private final JSObject esmangle;
+    
+    private final JSObject JSON;
 
     public Obfuscator() throws FileNotFoundException, ScriptException {
         this.nashornEngine = new ScriptEngineManager().getEngineByName("nashorn");
-        this.esprima = (JSObject) nashornEngine.eval(new FileReader("libs/esprima.js"));
-        this.escodegen = (JSObject) nashornEngine.eval(new FileReader("libs/escodegen.js"));
-        this.esmangle = null;
+
+        nashornEngine.eval(new FileReader("libs/esprima.js"));
+        nashornEngine.eval(new FileReader("libs/escodegen.js"));
+        nashornEngine.eval(new FileReader("libs/esmangle.js"));
+        
+        this.esprima = (JSObject) nashornEngine.get("esprima");
+        this.escodegen = (JSObject) nashornEngine.get("escodegen");
+        this.esmangle = (JSObject) nashornEngine.get("esmangle");
+        this.JSON = (JSObject) nashornEngine.get("JSON");
     }
     
     /**
@@ -39,9 +48,20 @@ final public class Obfuscator {
      * @param code
      * @param obfuscatingOptions
      * @return 
+     * @throws javax.script.ScriptException 
+     * @throws java.lang.NoSuchMethodException 
      */
-    public String obfuscateCode(String code, Properties obfuscatingOptions) {
-        throw new UnsupportedOperationException();
+    public String obfuscateCode(String code, Properties obfuscatingOptions) 
+            throws ScriptException, NoSuchMethodException {
+        Invocable inv = (Invocable) nashornEngine;
+        
+        Object tree = inv.invokeMethod(esprima, "parse", code);
+        Object mangledTree = inv.invokeMethod(esmangle, "mangle", tree);
+        
+        Object generatingOptions = inv.invokeMethod(JSON, "parse", 
+                "{\"format\":{ \"indent\":{ \"style\": \"\", \"base\": 0}, \"quotes\": \"auto\", \"compact\": true}}");
+        Object minifiedCode = inv.invokeMethod(escodegen, "generate", mangledTree, generatingOptions);
+        return (String) minifiedCode;
     }
-
+    
 }
